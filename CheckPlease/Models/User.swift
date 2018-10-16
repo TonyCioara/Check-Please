66705693866933d02eaa94b9e6d82e65311a4186
ErikPerez312 <theerikperez@gmail.com>
2018-10-16 15:04:24 -0700

    Add - ability to cache user, and refactored property types

--- CheckPlease/Models/User.swift
+++ CheckPlease/Models/User.swift
@@ -10,11 +10,11 @@ import Foundation
 
 struct User {
     
-    var email: String
-    var username: String
-    var firstName: String
-    var lastName: String
-    var phoneNumber: String
+    let username: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let phoneNumber: String
     
     init(email: String, username: String, firstName: String, lastName: String, phoneNumber: String) {
         self.email = email
@@ -24,4 +24,43 @@ struct User {
         self.phoneNumber = phoneNumber
     }
     
+    init?(json: [String: String]) {
+        // TODO: Verify correct keys for json response
+        guard
+            let username = json["username"],
+            let firstName = json["first_name"],
+            let lastName = json["last_name"],
+            let email = json["email"],
+            let phoneNumber = json["phone_number"] else {
+                return nil
+        }
+        
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phoneNumber = phoneNumber
+    }
+    
+    func cache() -> Bool {
+        let fileURL = FileManager.default
+            .urls(for: .cachesDirectory, in: .allDomainsMask)
+            .first!
+            .appendingPathComponent("user.plist")
+        return dictionary.write(to: fileURL, atomically: true)
+    }
+    
+    // MARK: - Private
+    
+    /// NSDictionary representation of user object used to facilitate caching to
+    /// document directory.
+    private var dictionary: NSDictionary {
+        return [
+            "username": username,
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email,
+            "phoneNumber": phoneNumber
+        ]
+    }
 }
